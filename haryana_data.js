6f06ac9b270a2325bc36f76744d8a7edfed15eed
aarushiyadav123-create <aarushi.yadav123@gmail.com>
2026-08-19 01:32:38 +0530

    Create haryana_data.js

---
+++ haryana_data.js
@@ -0,0 +1,32 @@
+const haryanaFullDB = {
+    "CHARKHI DADRI": {
+        "CHARKHI DADRI SUB-DIVISION": {
+            "BAUND": [
+                "Achina", "Akbarpur", "Balgarh", "Bas Ranila", "Baund Kalan", 
+                "Baund Khurd", "Bhagwi", "Dhani Phogat", "Gharethi", "Hindol", 
+                "Kalyana", "Mankawas", "Phogat", "Ranila", "Sanjarwas", "Sankrod"
+            ],
+            "CHARKHI DADRI": [
+                "Adalpur", "Bhiwani Gate", "Birhi Kalan", "Birhi Khurd", "Chhapar", 
+                "Dadri Rural", "Iri", "Jhojhu Kalan", "Kachri", "Khedi Bura", 
+                "Mehrana", "Ramalwas", "Samaspur", "Sarni", "Tikan Kalan"
+            ]
+        },
+        "BADHRA SUB-DIVISION": {
+            "BADHRA": [
+                "Badhra", "Chandwas", "Chhapar", "Ghuiyani", "Hansiwas", 
+                "Jhojhu Khurd", "Kadma", "Kallana", "Kheri Bura", "Lawa", 
+                "Mandola", "Nindana", "Penchead", "Surpura"
+            ]
+        }
+    },
+    "ROHTAK": {
+        "SAMPLA SUB-DIVISION": {
+            "SAMPLA": [
+                "Atail", "Balku", "Bhadhadu", "Chandi", "Dattaur", "Gandhra", 
+                "Ghashi", "Gijhi", "Ismaila 11B", "Ismaila 9B", "Kheri Sampla", 
+                "Kultana", "Naya Bans", "Nonand", "Rohad", "Sampla"
+            ]
+        }
+    }
+};
