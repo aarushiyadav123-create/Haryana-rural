6da746af3dcce5f1b2f0064d526f277f91939e06
aarushiyadav123-create <aarushi.yadav123@gmail.com>
2026-08-21 21:12:17 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,24 @@
-// firebase-config.js
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "YOUR_ACTUAL_API_KEY", // 👈 अपनी API Key यहाँ डालें
-  authDomain: "haryanagramin-7ee4a.firebaseapp.com",
-  databaseURL: "https://haryanagramin-7ee4a-default-rtdb.firebaseio.com", // Realtime Database URL
-  projectId: "haryanagramin-7ee4a",
-  storageBucket: "haryanagramin-7ee4a.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDMYacgjfb6gW3J5lv7Jyu3bpJenpL0Nzo",
+    authDomain: "mera-gaov.firebaseapp.com",
+    databaseURL: "https://mera-gaov-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mera-gaov",
+    storageBucket: "mera-gaov.firebasestorage.app",
+    messagingSenderId: "574021786245",
+    appId: "1:574021786245:web:7f4c042f347d0cab54a7ec",
+    measurementId: "G-757L4D3657"
+  };
 
-// Initialize Firebase & Realtime Database
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
